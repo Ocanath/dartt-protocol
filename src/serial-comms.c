7a85ac460410620f1635acf6f132b3f6d00cda27
ocanath <ocanath@gmail.com>
2025-07-17 13:55:34 -0700

    implemented the struct->buf function and helpers for loading the index arg. comments for the reverse function

--- src/serial-comms.c
+++ src/serial-comms.c
@@ -71,7 +71,7 @@ int create_misc_read_message(unsigned char address, uint16_t index, uint16_t num
 
     int cur_byte_index = 0;
     output->buf[cur_byte_index++] = address;    //byte 0 loaded
-    index = index | 0x8000; //set the read bit always
+    index = index | READ_WRITE_BITMASK; //set the read bit always
     unsigned char * p_index_word = (unsigned char *)(&index);
     output->buf[cur_byte_index++] = p_index_word[0];    //byte 1 loaded
     output->buf[cur_byte_index++] = p_index_word[1];    //byte 2 loaded
@@ -89,14 +89,106 @@ int create_misc_read_message(unsigned char address, uint16_t index, uint16_t num
     return cur_byte_index;
 }
 
-/*format an output buffer_t based on a serial message_t structure*/
-int misc_message_to_serial_buf(misc_message_t * msg, serial_message_type_t type, buffer_t * output)
+/*
+	Helper function to set the read/write bit argument of the index parameter in the message_t. 
+	If paired with set_index, can be used in any order.
+	It will overwrite the MSB of the index argument.
+*/
+int set_rw(misc_message_t * msg, read_write_type_t read_write)
 {
-
+	if(read_write == READ_MESSAGE)
+	{
+		msg->rw_index |= READ_WRITE_BITMASK;
+	}
+	else
+	{
+		msg->rw_index &= ~READ_WRITE_BITMASK;
+	}
+	return SERIAL_PROTOCOL_SUCCESS;
 }
 
-/*load a misc_message_t based on a serial input buffer */
-int serial_buf_to_misc_message(buffer_t * input, serial_message_type_t type, misc_message_t * msg)
+/*
+	Helper function to load the index portion of the rw_index argument of a misc_message.
+	
+*/
+int set_index(misc_message_t * msg, uint16_t index)
+{
+	if(msg == NULL)
+	{
+		return ERROR_INVALID_ARGUMENT;
+	}
+	msg->rw_index &= READ_WRITE_BITMASK;	//zero out all bits below the R/W bit
+	msg->rw_index |= (index & (~READ_WRITE_BITMASK));	//or-in all nonzero bits of the lowest 15 bits of the argument. This effectively copies in the index without overwriting the rw bit, allowing order independent calling of these functions
+	return SERIAL_PROTOCOL_SUCCESS;
+}
+
+/*
+	Format an output buffer_t based on a serial message_t structure.
+	You must load the msg address, rw_index, and payload fields properly
+*/
+int misc_message_to_serial_buf(misc_message_t * msg, serial_message_type_t type, buffer_t * output)
+{
+	if(msg == NULL || output == NULL)
+	{
+		return ERROR_INVALID_ARGUMENT;
+	}
+	if(type == TYPE_UART_MESSAGE)
+	{
+		if( (msg->payload.len + NUM_BYTES_NON_PAYLOAD) > output->size)	//we'll write an address, two index, and a crc. Precheck for overrun
+		{
+			return ERROR_MEMORY_OVERRUN;
+		}
+
+		int bidx = 0;
+		output->buf[bidx++] = msg->address;
+		//implement little-endian loading op for maximum portability. we always use little endian due to mainly using this on little endian systems.
+		output->buf[bidx++] = (unsigned char)(msg->rw_index & 0x00FF);	
+		output->buf[bidx++] = (unsigned char)((msg->rw_index & 0xFF00) >> 8);	
+		if(msg->rw_index & READ_WRITE_BITMASK == 0)	//iff this is a write message, we need to copy the msg->payload. Otherwise we can fall through and simply load the crc, then exit
+		{
+			for(int i = 0; i < msg->payload.len; i++)
+			{
+				output->buf[bidx++] = msg->payload.buf[i];
+			}
+		}
+		output->buf[bidx++] = get_crc16(output->buf, bidx);
+		output->len = bidx;
+		return SERIAL_PROTOCOL_SUCCESS;
+	}
+	else	//fall through to simple payload copying if you aren't a UART message. This can be handled outside this function trivially in a true CAN implementation for speed
+	{
+		if(msg->payload.len > output->size)
+		{
+			return ERROR_MEMORY_OVERRUN;
+		}
+		for(int i = 0; i < msg->payload.len; i++)
+		{
+			output->buf[i] = msg->payload.buf[i];
+		}
+		output->len = msg->payload.len;
+		return SERIAL_PROTOCOL_SUCCESS;
+	}
+}
+
+/*load a misc_message_t based on a serial input buffer.
+	ARGUMENTS:
+		input: the serial message itself
+		type: flag to indicate if we need to parse out the crc and address. If it is a CAN type message the address is ignored (whatever is in msg falls through)
+		mem_base: 
+			1. 	If valid, this is treated as the 'base pointer' for the memory we're writing, asuming we are getting a write request as a reciever.
+				We will then set the msg->payload.buf pointer as an offset to mem_base and copy the payload section from the input to the target. 
+				IMPORTANT: If this is valid, msg->payload MUST be NULL
+			2. 	If invalid (NULL or ->size = 0), this is ignored. The function will then copy the payload directly to the buffer msg->payload (zero aligned). This 
+				allows us to double-buffer read messages if desired. 
+				IMPORTANT: if the intent is to use a pre-setup msg->payload, you MUST set mem_base to NULL
+		msg: the misc_message from the buffer_t
+			msg->address will contain the message address, IFF it is not a CAN type message (otherwise, the address is not touched and falls through - it is assumed to be properly loaded beforehand)
+			msg->index will contain the message index argument, including the MSB read/write bit
+			msg->payload should be invalid (NULL or ->size = 0) when this function is called if mem_base is valid - it will then be assigned to an offset from mem_base, according to the input message content
+			msg->payload should be valid (points to valid memory region) when this function is called, and the intended use is to simply make a copy of the input buffer payload.
+	Note-pointer logic does not apply to 'read' type messages. If the message type is 'read', the payload pointers are simply ignored and they fall through
+ */
+int serial_buf_to_misc_message(buffer_t * input, serial_message_type_t type, buffer_t * mem_base, misc_message_t * msg)
 {
 	
 }
@@ -181,7 +273,7 @@ int parse_misc_command(buffer_t * input, serial_message_type_t type, buffer_t * 
 
     uint16_t index_argument;
     index_argument = (input->buf[NUM_BYTES_ADDRESS + 1] << 8) | input->buf[NUM_BYTES_ADDRESS];
-    uint16_t read_mask = index_argument & 0x8000;
+    uint16_t read_mask = index_argument & READ_WRITE_BITMASK;
     uint16_t index = index_argument & 0x7FFF;
     uint32_t byte_index = (uint32_t)(index*sizeof(uint32_t));
     
--- src/serial-comms.h
+++ src/serial-comms.h
@@ -3,6 +3,29 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/*
+	TODO: put the information in this comment into the README
+	
+	WE ARE ALWAYS LITTLE ENDIAN.
+	This library is meant to be C-standard compliant, but to be used in a way which is technically NOT standard compliant (type punning).
+	Specifically, if you are targeting little-endian 32bit architectures, such as an ARM STM32, you *can* set this up so that the generic memory
+	regions you are reading and writing to are typedef structs (with the packed attribute if desired). This means you don't need any additional
+	parsing layer - once you load the memory from an incoming write request frame, it is immediately accessible via the struct.
+
+	The rationale is that:
+		1. if you control the target architecture and it never changes, this optimization will always work
+		2. DX improves drastically if you don't have to implement explicit parsing. Laying out your memory as a struct means you never have to write parsers for general settings messages - 
+			only for very tight and specific message types!
+*/
+
+/*
+	TODO:
+		unit test set_rw and set_index. Make sure they are order independent as desired
+		unit test the forward and reverse functions (msg to buf and buf to msg)
+		refactor the parse function to use the above
+		refactor the unit tests to use only the new functions; remove create_msic_write, create_misc_read, and parse_misc_command. Keep parse_general and index_of_field. Fix warnings in index_of_field - ensure casting is explicit
+*/
+
 #define NUM_BYTES_ADDRESS sizeof(unsigned char)
 #define NUM_BYTES_INDEX sizeof(uint16_t)
 #define NUM_BYTES_NUMWORDS_READREQUEST	sizeof(uint16_t)	//for a read struct request, we send a fixed 16bit integer argument in the payload section for the readsize request
@@ -14,9 +37,13 @@
 #define MASTER_MOTOR_ADDRESS	0x7F
 #define MASTER_MISC_ADDRESS		(0xFF - 0x7F)
 
-enum {ERROR_INVALID_ARGUMENT = -4, ERROR_CHECKSUM_MISMATCH = -3, ERROR_MALFORMED_MESSAGE = -2, ADDRESS_FILTERED = -1, SERIAL_PROTOCOL_SUCCESS = 0};
+#define READ_WRITE_BITMASK	0x8000	//msg is the read write bit. 1 for read, 0 for write.
+
+enum {ERROR_MEMORY_OVERRUN = -5, ERROR_INVALID_ARGUMENT = -4, ERROR_CHECKSUM_MISMATCH = -3, ERROR_MALFORMED_MESSAGE = -2, ADDRESS_FILTERED = -1, SERIAL_PROTOCOL_SUCCESS = 0};
 
 typedef enum {TYPE_UART_MESSAGE = 0, TYPE_CAN_MESSAGE = 1} serial_message_type_t;	
+
+typedef enum {WRITE_MESSAGE, READ_MESSAGE} read_write_type_t;
 
 typedef struct buffer_t
 {
@@ -28,8 +55,8 @@ typedef struct buffer_t
 typedef struct misc_message_t
 {
 	uint8_t address;	//the address
-	uint16_t index;	//MSB is read (1) / write (0)
-	buffer_t * payload;	//the content of the messasge
+	uint16_t rw_index;	//MSB is read (1) / write (0). The low 15 bits are an index argument, corresponding to a 4-byte index offset from a base pointer. memory is assumed to be 32-bit aligned
+	buffer_t payload;	//the content of the messasge
 	//the checksum is computed in the message loader function, iff the hardware doesn't support it inherently. Therefore it is considered 'user alterable' data and not part of the message structure.
 }misc_message_t;
 
