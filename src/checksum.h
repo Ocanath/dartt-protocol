98a5d047ecf4ae955e2d4ebadbc3c7db39e41336
Jesse Cornman <ocanath@gmail.com>
2025-09-21 22:54:47 -0700

    c++ extern macro logic

--- src/checksum.h
+++ src/checksum.h
@@ -3,8 +3,16 @@
 
 #include <stdint.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint16_t get_checksum16(uint16_t* arr, size_t size);
 uint16_t get_crc16(uint8_t* arr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
